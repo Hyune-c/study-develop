55bf209de89f148ca4de3d7c96e844e1ce1b30be
hyune <choi8608@gmail.com>
2021-09-19 04:18:26 +0900

    [spring-complete-package] ✨ dmaker init

--- spring-complete-package/README.md
+++ spring-complete-package/README.md
@@ -1,3 +1,8 @@
 # 한 번에 끝내는 Spring 완.전.판 초격차 패키지 Online
 
 [Link](https://fastcampus.co.kr/dev_online_spring)
+
+### docs
+
+- spring framework 기본 이론
+
---
+++ spring-complete-package/dmaker/src/main/java/com/example/dmaker/DmakerApplication.java
@@ -0,0 +1,12 @@
+package com.example.dmaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmakerApplication {
+
+	public static void main(final String[] args) {
+		SpringApplication.run(DmakerApplication.class, args);
+	}
+}
